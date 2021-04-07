1361b2f2acb1b82818d06b273eae9fce0b17d419
Marijn Gaikema <marijngaikema@gmail.com>
2021-04-07 17:21:14 +0200

    added firebase config values to env file

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,12 +4,12 @@ import 'firebase/firestore'
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-	apiKey: 'AIzaSyD_JSzGjKGywGuQaeI_rnA8l_ESD5Q4byI',
-	authDomain: 'firegram-1fe0c.firebaseapp.com',
-	projectId: 'firegram-1fe0c',
-	storageBucket: 'firegram-1fe0c.appspot.com',
-	messagingSenderId: '782015232997',
-	appId: '1:782015232997:web:9c4fb167b571d3793dcd0c',
+	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+	projectId: process.env.REACT_APP_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+	appId: process.env.REACT_APP_APP_ID,
 }
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig)
